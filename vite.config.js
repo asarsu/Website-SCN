e91096412672d0bfe989b0cb77f23eca7f6777cd
Andres Sarmiento <asarmientosuarez@gmail.com>
2025-10-10 21:01:26 -0400

    version naranja con mobile first hasta seccion eventos

--- vite.config.js
+++ vite.config.js
@@ -4,22 +4,23 @@ import { resolve } from 'path'
 
 export default defineConfig({
  
-  base: '/HTML-CSS-Proyecto_Final_TransGlobal_web/',
+
+  base: '/soc-web-page/',
  
 
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        nosotros: resolve(__dirname, 'nosotros.html'),
-        servicios: resolve(__dirname, 'servicios.html'),
-        contacto: resolve(__dirname, 'contacto.html'),
-        home: resolve(__dirname, 'home.html'),
-        about: resolve(__dirname, 'about.html'),
-        services: resolve(__dirname, 'services.html'),
-        contact: resolve(__dirname, 'contact.html'),
-        gracias: resolve(__dirname, 'gracias.html'),
-        thanks: resolve(__dirname, 'thanks.html'),
+        // nosotros: resolve(__dirname, 'nosotros.html'),
+        // servicios: resolve(__dirname, 'servicios.html'),
+        // contacto: resolve(__dirname, 'contacto.html'),
+        // home: resolve(__dirname, 'home.html'),
+        // about: resolve(__dirname, 'about.html'),
+        // services: resolve(__dirname, 'services.html'),
+        // contact: resolve(__dirname, 'contact.html'),
+        // gracias: resolve(__dirname, 'gracias.html'),
+        // thanks: resolve(__dirname, 'thanks.html'),
       },
     }
   
